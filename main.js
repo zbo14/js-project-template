b80d23706431e8013494bf82a1fc4a880abbd376
zach balder <zmbalder@gmail.com>
2022-10-09 01:42:06 -0400

    helper functions, app styles

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 import './style.css'
 
-console.log('Hello world')
+// const create = tag => document.createElement(tag)
+const query = selector => document.querySelector(selector)
+// const queryAll = selector => document.querySelectorAll(selector)
+// const style = (element, styles) => Object.assign(element.style, styles)
+
+const app = query('#app')
+
+app.innerText = 'Hello world'
